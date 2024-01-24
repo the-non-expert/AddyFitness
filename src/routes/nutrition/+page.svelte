eebb1572a4c66f05e3761e4bdb0f038c668db7ac
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-01-24 17:04:08 +0530

    nutrition page done.

--- src/routes/nutrition/+page.svelte
+++ src/routes/nutrition/+page.svelte
@@ -1,1 +1,126 @@
-<div class="text-white">Addy Fitness</div>
+<script>
+  import WeightLoss from "$lib/images/weightLoss.gif";
+  import WeightGain from "$lib/images/weightGain.gif";
+  import diabetes from "$lib/images/diabetes.gif";
+  import Thyroid from "$lib/images/thyroid.gif";
+  import PCOS from "$lib/images/PCOS.jpeg";
+  import Pregnancy from "$lib/images/pregnancy.gif";
+  import BloodPressure from "$lib/images/bloodPressure.gif";
+  import kidney from "$lib/images/kidney.gif";
+  import ChildNutrition from "$lib/images/ChildNutrition.gif";
+  import Live from "$lib/images/zumba.gif";
+  import NutritionCard from "./nutritionCard.svelte";
+
+  const nutritionServices = [
+    {
+      id: 1,
+      name: "Weight Loss",
+      gifSrc: WeightLoss,
+      planDuration: "2 Months Plan",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 2,
+      name: "Weight Gain",
+      gifSrc: WeightGain,
+      planDuration: "2 Months Plan",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 3,
+      name: "Diabetes",
+      gifSrc: diabetes,
+      planDuration: "As per subsription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 4,
+      name: "Thyroid",
+      gifSrc: Thyroid,
+      planDuration: "As per subscription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 5,
+      name: "PCOS/PCOD",
+      gifSrc: PCOS,
+      planDuration: "As per subscription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 6,
+      name: "Gestation (Pregnancy )",
+      gifSrc: Pregnancy,
+      planDuration: "9 Months/ 39 Weeks",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 7,
+      name: "Post Pregnancy",
+      gifSrc: Live,
+      planDuration: "1 year post pregnancy",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 8,
+      name: "Senior Nutrition",
+      gifSrc: ChildNutrition,
+      planDuration: "As per subscription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 9,
+      name: "Blood Pressure",
+      gifSrc: BloodPressure,
+      planDuration: "As per subscription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 10,
+      name: "Renal Function (Kidney)",
+      gifSrc: kidney,
+      planDuration: "As per subscription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 11,
+      name: "Child Nutrition)",
+      gifSrc: ChildNutrition,
+      planDuration: "Yearly Plan",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+    {
+      id: 10,
+      name: "Anaemia",
+      gifSrc: kidney,
+      planDuration: "As per subsription",
+      frequency: "Workout Plan Also Available",
+      progressTracking: "Progress Tracking Weekly",
+    },
+  ];
+</script>
+
+<div class="text-white mt-6 grid grid-cols-2 gap-x-10 gap-y-4">
+  {#each nutritionServices as item (item.id)}
+    <NutritionCard
+      title={item.name}
+      host="with Adnan Ali"
+      buttonText="Get your Nutrition Chart"
+      gifSrc={item.gifSrc}
+      time={item.planDuration}
+      calorie={item.progressTracking}
+      frequency={item.frequency}
+    />
+  {/each}
+</div>
---
+++ src/routes/nutrition/nutritionCard.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let title = "Workout Session";
+  export let host = "Workout Session";
+  export let buttonText = "Start Workout";
+  export let gifSrc = "";
+  export let time = "60 mins";
+  export let calorie = "300-400";
+  export let frequency = "5 Sessions a Week";
+
+  import timerBlack from "$lib/images/timerBlack.svg";
+  import calorieBlack from "$lib/images/calorieBlack.svg";
+  import sessionBlack from "$lib/images/sessionBlack.svg";
+  import bgVideo from "$lib/images/trainingCardBg.mp4";
+  import bgCards from "$lib/images/bgCards.png";
+</script>
+
+<div class={`text-black rounded-lg flex relative overflow-hidden`}>
+  <!-- Video background for the current card -->
+  <img src={bgCards} class="object-cover w-full h-full absolute" alt="" />
+
+  <!-- left part with the gif -->
+  <div class="w-6/12 z-10">
+    <img src={gifSrc} alt="gif" class="w-64 h-52 object-cover" />
+  </div>
+
+  <!-- right part with details and button -->
+  <div class="flex flex-col justify-evenly w-6/12 z-10 px-1">
+    <div>
+      <h2 class="text-xl font-bold text-red-500">{title}</h2>
+      <h2 class="">{host}</h2>
+    </div>
+
+    <div class="">
+      <div class="flex gap-2">
+        <img src={timerBlack} alt="calorieBlack" class="w-5" />
+        <p class="font-['Montserrat']">{time}</p>
+      </div>
+
+      <div class="flex gap-2">
+        <img src={calorieBlack} alt="calorieBlack" class="w-5" />
+
+        <p class="font-['Montserrat']">{calorie}</p>
+      </div>
+
+      <div class="flex gap-2">
+        <img src={sessionBlack} alt="sessionBlack" class="w-5" />
+
+        <p class="font-['Montserrat']">{frequency}</p>
+      </div>
+    </div>
+
+    <!-- Call to action button -->
+    <button
+      class={`bg-black text-white rounded-3xl h-8 w-64 cursor-pointer mr-2`}
+    >
+      {buttonText}
+    </button>
+  </div>
+</div>
