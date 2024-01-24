891d4076beae76eeb8213f4ec4344f18d8152ad8
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-01-25 00:25:08 +0530

    management page done.

--- src/routes/management/+page.svelte
+++ src/routes/management/+page.svelte
@@ -1,1 +1,39 @@
-<div class="text-white">Addy Fitness</div>
+<script>
+  import franchise from "$lib/images/franchise.gif";
+  import managingPartner from "$lib/images/managingPartner.gif";
+  import namingPartner from "$lib/images/namingPartner.gif";
+  import ManagementCard from "./managementCard.svelte";
+
+  const managementServices = [
+    {
+      id: 1,
+      name: "Naming Partner",
+      gifSrc: namingPartner,
+      supportText: "WE HELP YOU GROW",
+    },
+    {
+      id: 2,
+      name: "Managing Partner",
+      gifSrc: managingPartner,
+      supportText: "LET'S GROW TOGETHER",
+    },
+    {
+      id: 3,
+      name: "Franchise",
+      gifSrc: franchise,
+      supportText: "LET'S MAKE A COMMUNITY TOGETHER",
+    },
+  ];
+</script>
+
+<div class="text-white mt-6 flex flex-col gap-10 w-full">
+  {#each managementServices as item (item.id)}
+    <ManagementCard
+      title={item.name}
+      host=""
+      buttonText="Contact Us"
+      gifSrc={item.gifSrc}
+      supportText={item.supportText}
+    />
+  {/each}
+</div>
---
+++ src/routes/management/managementCard.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let title = "Workout Session";
+  export let host = "Workout Session";
+  export let buttonText = "Start Workout";
+  export let gifSrc = "";
+  export let supportText = "";
+  import bgCards from "$lib/images/bgCards.png";
+</script>
+
+<div class={`text-black rounded-lg flex relative overflow-hidden`}>
+  <!-- Video background for the current card -->
+  <img src={bgCards} class="object-cover w-full h-full absolute" alt="" />
+
+  <!-- left part with the gif -->
+  <div class=" z-10">
+    <img src={gifSrc} alt="gif" class="w-64 h-52 object-cover" />
+  </div>
+
+  <!-- right part with details and button -->
+  <div class="flex flex-col justify-evenly items-center z-10 px-1">
+    <div>
+      <h2 class="text-xl font-bold text-red-500">{title}</h2>
+      <h2 class="">{host}</h2>
+    </div>
+
+    <div class="text-center">
+      <p>{supportText}</p>
+    </div>
+
+    <!-- Call to action button -->
+    <button
+      class={`bg-black text-white rounded-3xl h-8 w-64 cursor-pointer ml-5`}
+    >
+      {buttonText}
+    </button>
+  </div>
+</div>
