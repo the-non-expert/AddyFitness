2eae74b8cecb9ab2657ba5bfc9ccdf900487d355
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-01-25 00:02:28 +0530

    healthcare page done.

--- src/routes/healthcare/+page.svelte
+++ src/routes/healthcare/+page.svelte
@@ -1,1 +1,65 @@
-<div class="text-white">Addy Fitness</div>
+<script>
+  import emergency from "$lib/images/emergency.gif";
+  import generalPhysician from "$lib/images/generalPhysician.gif";
+  import medicineSpecialist from "$lib/images/medicineSpecialist.gif";
+  import Thyroid from "$lib/images/thyroid.gif";
+  import psychology from "$lib/images/psychology.gif";
+  import Pregnancy from "$lib/images/pregnancy.gif";
+  import BloodPressure from "$lib/images/bloodPressure.gif";
+  import kidney from "$lib/images/kidney.gif";
+  import ChildNutrition from "$lib/images/ChildNutrition.gif";
+  import Live from "$lib/images/zumba.gif";
+  import NutritionCard from "./healthcareCard.svelte";
+  import HealthcareCard from "./healthcareCard.svelte";
+
+  const nutritionServices = [
+    {
+      id: 1,
+      name: "Emergency",
+      gifSrc: emergency,
+      supportText: "GET INSTANT EMERGENCY HELP FROM OUR DOCTORS",
+    },
+    {
+      id: 2,
+      name: "General Physician",
+      gifSrc: generalPhysician,
+      supportText: "CONSULT ABOUT ANY PROBLEM",
+    },
+    {
+      id: 3,
+      name: "Medicine Specialist",
+      gifSrc: medicineSpecialist,
+      supportText: "CONSULT SPECIFIC RELATED DISORDER",
+    },
+    {
+      id: 4,
+      name: "Gynaecologist",
+      gifSrc: Pregnancy,
+      supportText: "CONSULT GYNIC RELATED ISSUES CONFIDENTIALLY",
+    },
+    {
+      id: 5,
+      name: "Endocronologist",
+      gifSrc: Thyroid,
+      supportText: "GET REVERSE YOUR DIABETES AND THYROID AT HOME.",
+    },
+    {
+      id: 6,
+      name: "Psychologist",
+      gifSrc: psychology,
+      supportText: "GET A FRIEND TO CURE YOURSELF",
+    },
+  ];
+</script>
+
+<div class="text-white mt-6 grid grid-cols-2 gap-x-10 gap-y-4">
+  {#each nutritionServices as item (item.id)}
+    <HealthcareCard
+      title={item.name}
+      host=""
+      buttonText="Book an Appointment"
+      gifSrc={item.gifSrc}
+      supportText={item.supportText}
+    />
+  {/each}
+</div>
---
+++ src/routes/healthcare/healthcareCard.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let title = "Workout Session";
+  export let host = "Workout Session";
+  export let buttonText = "Start Workout";
+  export let gifSrc = "";
+  export let supportText = "";
+  import timerBlack from "$lib/images/timerBlack.svg";
+  import calorieBlack from "$lib/images/calorieBlack.svg";
+  import sessionBlack from "$lib/images/sessionBlack.svg";
+  import bgVideo from "$lib/images/trainingCardBg.mp4";
+  import bgCards from "$lib/images/bgCards.png";
+</script>
+
+<div class={`text-black rounded-lg flex relative overflow-hidden`}>
+  <!-- Video background for the current card -->
+  <img src={bgCards} class="object-cover w-full h-full absolute" alt="" />
+
+  <!-- left part with the gif -->
+  <div class="w-3/6 z-10">
+    <img src={gifSrc} alt="gif" class="w-64 h-52 object-cover" />
+  </div>
+
+  <!-- right part with details and button -->
+  <div class="flex flex-col justify-evenly items-center z-10 px-1">
+    <div>
+      <h2 class="text-xl font-bold text-red-500">{title}</h2>
+      <h2 class="">{host}</h2>
+    </div>
+
+    <div class="text-center">
+      <p>{supportText}</p>
+    </div>
+
+    <!-- Call to action button -->
+    <button
+      class={`bg-black text-white rounded-3xl h-8 w-64 cursor-pointer mr-2`}
+    >
+      {buttonText}
+    </button>
+  </div>
+</div>
